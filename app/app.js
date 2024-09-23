e99adfd7985d96155978ea62ec7ba1935f0e5a53
mahbubsakib <sakibmahbub92@gmail.com>
2024-09-23 14:44:20 +0600

    index page

---
+++ app/app.js
@@ -0,0 +1,7 @@
+document.getElementById('btn-noakhali').addEventListener('click', function() {
+    const inputNoakhali = document.getElementById('input-noakhali').value;
+    
+
+    console.log(typeof inputNoakhali);
+});
+
